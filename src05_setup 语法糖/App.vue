966f8caf859210cf24c9f65bb018bfd320d53ca2
chanyuan <liuchanyuan12@126.com>
2025-03-07 00:00:47 +0800

    setup语法糖

---
+++ src05_setup 语法糖/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <Person /> <!-- 使用 -->
+</template>
+<script lang="ts">
+import Person from './components/Person.vue'; // 引入--这句
+export default {
+  name: "App",
+  components: { Person }  // 注册组件
+}
+</script>
+<style></style>
