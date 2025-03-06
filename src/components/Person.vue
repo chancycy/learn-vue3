b85a2ff14aecd68390665853d251bbea101c0ed7
chanyuan <liuchanyuan12@126.com>
2025-03-06 23:22:52 +0800

    去掉上一节使用到的测试代码

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -5,29 +5,11 @@
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">年龄+1</button>
         <button @click="showTel">点我查看联系方式</button>
-        <hr>
-        <h2>测试1：{{ a }}</h2>
-        <h2>测试2：{{ c }}</h2>
-        <h2>测试3：{{ d }}</h2>
-        <button @click="b">测试</button>
     </div>
 </template>
 <script lang="ts">
 export default {
     name: "Person",
-    data() {
-        return {
-            a: 100,
-            c: this.name,   // 能读取到setup中的name,页面会显示'张三'
-            d: 900,
-            age: 90 // 页面显示的age是setup中的age
-        }
-    },
-    methods: {
-        b() {
-            console.log('b')
-        }
-    },
     setup() {
         console.log(this) //undefined
 
